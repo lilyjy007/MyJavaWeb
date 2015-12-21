27d7e140cf090ad0e8ef5a38e1245bbaa5d41b1c
lilijy007 <2232701617@qq.com>
2015-12-21 15:32:16 +0800

    首次提交

---
+++ src/main/java/example0121/GeneralController.java
@@ -0,0 +1,13 @@
+package example0121;
+
+import org.springframework.stereotype.Controller;  
+import org.springframework.ui.Model;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+
+public class GeneralController {
+	@RequestMapping(value="index.do")  
+    public void index_jsp(Model model){  
+        model.addAttribute("str0121", "Hellow world");  
+        System.out.println("index.jsp");  
+    }  
+}
